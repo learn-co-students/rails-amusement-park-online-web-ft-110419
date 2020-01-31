23361de1a43a42e6e98051c894e317d7ca6daa18
jessesbyers <jesse.s.byers@gmail.com>
2020-01-31 14:49:56 -0500

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -10,7 +10,6 @@ class AttractionsController < ApplicationController
         redirect_to attraction_path(@attraction)
     end
  
- 
     def index
         @attractions = Attraction.all
         @user = User.find(session[:user_id])
@@ -18,6 +17,16 @@ class AttractionsController < ApplicationController
 
     def show
         @attraction = Attraction.find(params[:id])
+        @user = User.find(session[:user_id])
+    end
+
+    def edit
+        @attraction = Attraction.find(params[:id])
+    end
+
+    def update
+        @attraction.update(attraction_params)
+        redirect_to attraction_path(@attraction)
     end
 
     private
